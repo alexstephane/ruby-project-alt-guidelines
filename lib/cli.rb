f8db6e02e55714500fe1621c24a4292f1ae5a26a
alexstephane <alexngundji@gmail.comm.com>
2020-09-12 15:48:29 -0400

    wip

--- lib/cli.rb
+++ lib/cli.rb
@@ -42,7 +42,7 @@ def create_account
             
                 
                
-               user_menu(user)
+  user_menu(user)
         
 end
 
@@ -180,43 +180,4 @@ end
 
     
 
-        
-
-    # def run 
-    #     name= welcome
-    #     find=find_name
-    #     puts name
-
-        
-        
-        
-    #     login_or_sign_up
-    #     action1 #As a user,  I want to enter a year and receive a list of movies for that year. 
-    #     action2 #As a user, I want to be able to checkout a movie(create/update)
-    #     action3 #As a user, I want to get rating above a certain number(read)
-    #     action4 #As a user, i want a list of all movies checked (History of transactions) 
-    #     action5 #As a user, I want to edit my user_name  or name(update)
-    #     action6 #As a user I can delete my account (delete)
-
-
-#      end
-
-    
-
-     
-# run
-
-   
-        
-
-    
-
-
-
-# def existsCheck( db, id )
-#     temp = db.execute( "select 1 where exists(
-#         select 1
-#         from Products
-#         where promoID = ?
-#     ) ", [id] ).any?
-# end
+      
